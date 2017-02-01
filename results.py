6b6cb4e84e86a4e2df5504d6f082ac19e57cbbb7
James Fowkes <jamesfowkes@gmail.com>
2017-02-01 20:49:19 +0000

    Fix missing last result bug

--- results.py
+++ results.py
@@ -6,6 +6,7 @@ Usage:
 """
 
 import docopt
+import logging
 
 def split_into_results(lines):
 
@@ -19,6 +20,8 @@ def split_into_results(lines):
 		else:
 			result.append(line)
 
+	results.append(result)
+	
 	return results
 
 def parse_for_results(f):
@@ -36,6 +39,8 @@ def get_name(result):
 
 if __name__ == "__main__":
 
+	logging.basicConfig(level=logging.INFO)
+
 	args = docopt.docopt(__doc__)
 
 	filepath = args["<blast_file>"]
@@ -43,5 +48,9 @@ if __name__ == "__main__":
 	with open(filepath, 'r') as f:
 		results = parse_for_results(f)
 
+	logging.getLogger(__name__).info("Found {} sequences".format(len(results)))
+	logging.getLogger(__name__).info("First: {}".format(get_name(results[0])))
+	logging.getLogger(__name__).info("Last: {}".format(get_name(results[-1])))
+	
 	for res in results:
 		print(get_name(res))
